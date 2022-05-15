ec6754f76d160e6efe430bc45f8882c29da6f74b
hueychopper <codelearn370@gmail.com>
2022-05-15 14:42:39 -0400

    switch to testing 0; fix timely updates

--- main/script.js
+++ main/script.js
@@ -1,6 +1,6 @@
 import "./data.js";
 
-mapboxgl.accessToken = 'mapbox-token';
+mapboxgl.accessToken = 'pk.eyJ1IjoiYm9iLXVzZXIiLCJhIjoiY2tycDM0MjE4MGZsejJ1bXcwczNka3hnNSJ9.JlXSoboCjRDRyUaoHDeUSw';
 var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/dark-v10'
@@ -26,38 +26,44 @@ async function finalRequest() {
     );
     var data = await freq.json();
     let sort = groupData(data, 'trainId');
-    Object.keys(sort).map((key, index) => {
-        // if(updated == true) {
-            if(index == 0) {
-                for(let i = 0; i < sort[key].length; i++) {
-                    let temp_len = sort[key].length;
-                    const nel = document.createElement("div");
-                    const pid = document.createElement("p");
-                    const pstop = document.createElement("p");
-                    const ptime = document.createElement("p");
-                    pid.innerText = sort[key][i].trainId;
-                    pstop.innerText = sort[key][i].stop_name;
-                    ptime.innerText = sort[key][i].arrival;
-                    nel.className = "card"; pid.className = "pid"; pstop.className = "stop"; ptime.className = "time";
-                    nel.appendChild(pid); nel.appendChild(pstop); nel.appendChild(ptime);
-                    // if(sort[key].length <= temp_len) {
-                        maintab.appendChild(nel);
-                        setTimeout(() => {
-                            nel.remove();
-                        }, 5000)
-                    // }
-                }
-            }
-        // }
-        // else {
-        //     maintab.removeChild(nel);
-        //     updated = false;
-        // }
-    });
-    return data;
+    return sort;
 }
 setInterval(() => {
     console.log(dbundle._ot009);
-    // truncate();
-    finalRequest();
+    let dat = finalRequest();
+    dat.then((data) => {
+        let els = [];
+        let tester = Object.values(data)[0];
+        console.log(tester);
+        // Object.keys(tester).map((key, index) => {
+        // });
+        for(let i = 0; i < tester.length; i++) {
+            const nel = document.createElement("div");
+            const pid = document.createElement("p");
+            const pstop = document.createElement("p");
+            const ptime = document.createElement("p");
+            pid.innerText = tester[i].trainId;
+            pstop.innerText = tester[i].stop_name;
+            ptime.innerText = tester[i].arrival;
+
+            nel.className = "card"; nel.id = "card";
+            pid.className = "pid"; 
+            pstop.className = "stop"; 
+            ptime.className = "time";
+
+            nel.appendChild(pid); 
+            nel.appendChild(pstop); 
+            nel.appendChild(ptime);
+
+            els.push(nel);
+        }
+        // console.log(els);
+        let uniques = [...new Set(els)];
+        for(let i = 0; i < uniques.length; i++) {
+            maintab.appendChild(uniques[i]);
+        }
+    });
+    let i_d = document.getElementById("card");
+    // let uniques = [...new Set(els)];
+
 }, 15000);
