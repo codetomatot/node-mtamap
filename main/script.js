d67d98510d97c3a5ab4c6939b0aab0b62969d175
Student <student@ac1mwk0f6jc9h.78b505.nycboe.org>
2022-05-31 08:34:58 -0400

    functional

--- main/script.js
+++ main/script.js
@@ -31,150 +31,39 @@ async function finalRequest() {
     return sort;
 }
 
-const toHtml = data => data.map(({trainId, stop_name, arrival}, count) => {
-    return `<div class="card t${count.toString()}t" id="card">
-              <p class="pid">${trainId}</p>
-              <p class="stop">${stop_name}</p>
-              <p class="time"> ${arrival} </p>
-          </div>`
-  }).join('')
+const toMainHtml = (data, count) => {
+    return `<div class="trip-general t${count.toString()}t" id="card">
+                <p class="pid t${count.toString()}p">${data[0].trainId}</p>
+                <p class="stop t${count.toString()}s">${data[0].stop_name} -> ${data[data.length-1].stop_name}</p>
+            </div>`
+}
 
 function toStart_toRecover() {
     let fcall = finalRequest();
     let counter = 0;
     fcall.then((data) => {
+        let len = Object.keys(data).length;
         Object.keys(data).map((key, index) => {
-            if(data[key].length < 10) {
-                maintab.innerHTML += toHtml(data[key], counter);
-                counter++;
-                const divider = document.createElement("div");
-                divider.className = "divide";
-                maintab.appendChild(divider);
-            }
+            maintab.innerHTML += toMainHtml(data[key], counter);
+            counter++;
+            const divider = document.createElement("div");
+            divider.className = "divide";
+            maintab.appendChild(divider);
         });
     });
 }
 toStart_toRecover();
 
-const countOccur = (arr, value) => {
-    return arr.reduce((a, val) => (val === value ? a + 1 : a), 0);
-}
-function spliceArray(idtp, na) {
-    let spliced = [];
-    for(let j = 0; j < na.length; j++) {
-        spliced.push(idtp.splice(0, na[j].length)); 
-    }
-    return spliced;
-} 
-
-function removeDivide() {
-    let top = document.getElementById("placehold");
-    let dividers = [...document.querySelectorAll(".divide")];
-    let allChildren = [...maintab.children];
-    allChildren.shift();
-    
-    let temp_indices = dividers.map((divider) => allChildren.indexOf(divider));
-    const removeItem = (items, i) => items.slice(0, i-1).concat(items.slice(i, items.length));
-
-    let init = removeItem(allChildren, temp_indices[0]+1);
-    while(temp_indices.length != 0) {
-        temp_indices.shift();
-        temp_indices = temp_indices.map(val => val -= 1);
-        init = removeItem(init, temp_indices[0]+1);
-    }
-    return init;
-}
-function resetIntervals(temp_indices, na, allChildren) {
-    //intervals of stops between next stop and last stop, inclusive
-    let intervals = [];
-    let interval = allChildren.slice(0, temp_indices[0]); 
-    for(let i = 0; i < na.length; i++) {
-        intervals.push(interval);
-        interval = allChildren.slice(temp_indices[i]+1, temp_indices[i+1]);
-    }
-    return intervals;
-}
-
-
 setInterval(() => {
-    console.log(dbundle._ot009);
-    let dat = finalRequest();
-    let count = 0;
-    dat.then((data) => {
-        var na = [];
-        let keyholder = [];
+    let ncall = finalRequest();
+    let currentCount = 0;
+    ncall.then((data) => {
         Object.keys(data).map((key, index) => {
-            let i_d = document.querySelectorAll("#card");
-            let i_d_d = Array.from(i_d);
-            // let divides = document.querySelectorAll(".divide");
-            let idtp = [...document.querySelectorAll(".pid")];
-            let stoptp = document.getElementsByClassName("stop");
-            let timetp = document.getElementsByClassName("time");
-            if(data[key].length < 10) {
-                let modArr = [];
-                keyholder.push(data[key]);
-                for(let i = 0; i < data[key].length; i++) {
-                    modArr.push(i);
-                }
-                modArr.push("Q"); //extra end value for functionality
-
-                let index = modArr.indexOf(0);
-                for(let i = 0; i < countOccur(modArr, 0); i++) {
-                    na.push(modArr.slice(index, modArr.indexOf(0,index+1)));
-                    index = modArr.indexOf(0, index+1);
-                }
-                //start changing text content
-                // let idsToFill = spliceArray(idtp, na);
-                // if(idsToFill.length === na.length) {
-                //     // console.log(na);
-                //     // console.log(idsToFill);
-                //     for(let i = 0; i < idsToFill.length; i++) {
-                //         if(idsToFill[i].length == na[i].length) {
-                //             console.log("idstofill[i] is same as na[i] in length");
-                //             // for(let j = 0; j < idsToFill[i].length; j++) {
-                //             //     // idsToFill[i][j].innerText = "new value : "+ data[key][i].trainId;
-                //             //     console.log(data[key]);
-                //             // }
-                //         } else if(na[i].length > idsToFill[i].length) {
-                //             console.log("na is grater than ids");
-                //             idsToFill = spliceArray(idtp, na);
-                //         }
-                //     }
-                // } else {
-                //     console.log("so far not good");
-                // }
-
-
-                // let allnodes = removeDivide();
-
-                let dividers = [...document.querySelectorAll(".divide")];
-                let allChildren = [...maintab.children];
-                allChildren.shift();
-                let temp_indices = dividers.map((divider) => allChildren.indexOf(divider));
-
-                if(na.length == temp_indices.length && keyholder.length) {
-                    let ttda = spliceArray(i_d_d, na);
-                    let intervals = resetIntervals(temp_indices, na, allChildren);
-
-                    if(keyholder.length == intervals.length) {
-                        for(let i = 0; i < intervals.length; i++) {
-                            if(keyholder[i].length < intervals[i].length) {
-                                let diff = intervals[i].length - keyholder[i].length;
-                                if(diff == 1) {
-                                    let tr = intervals[i].shift();
-                                    console.log(keyholder[i]);
-                                    console.log(intervals[i]);
-                                    maintab.removeChild(tr);
-                                }
-                            }
-                            // if(na[i] > intervals[i]) ... ended trip
-                            console.log("/////////////////////////////////////////");
-                        }
-                    } else {
-                        console.log("ttda: "+ttda.length + " != " + "intervals: " + intervals.length);
-                    }
-                }
+            let cpid = document.querySelector(`.t${currentCount.toString()}s`);
+            if(cpid != null) {
+                cpid.innerText = data[key][0].stop_name + " -> " + data[key][data[key].length-1].stop_name;
             }
-        });
-    });
+            currentCount++;
+        })
+    })
 }, 15000);
